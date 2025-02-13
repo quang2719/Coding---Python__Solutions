234ee29f1efbe768f06c4b0426be13c7fbbb2214
Nguyen Viet Quang <111571873+quang2719@users.noreply.github.com>
2025-02-13 13:14:55 +0700

    new solution

--- Other/lc3066.py
+++ Other/lc3066.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def minOperations(self, nums, k):
         """
@@ -5,4 +6,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        heap = []
+        for i,x in enumerate(nums):
+            heapq.heappush(heap,(x,i))
+        count = 0
+        id = len(nums)
+        while len(heap) >= 2:
+            x,i = heapq.heappop(heap)
+            if x>=k: break
+            y,i = heapq.heappop(heap)
+            heapq.heappush(heap,( x * 2 +  y,id))
+            id+=1
+            count+=1
+        return count
+solul = Solution()
+nums = [2,11,10,1,3]
+k = 10
+print(solul.minOperations(nums,k))
         
+            
--- Other/test.py
+++ Other/test.py
@@ -1,4 +1,7 @@
-s = "asak43jh2dfkas"
-for x in s:
-    if x.isdigit():
-        print(x)
+import heapq
+ar = [1,2,4,5,6,4,23]
+heap = []
+for x in ar:
+    heapq.heappush(heap,x)
+while heap:
+    print(heapq.heappop(heap))
